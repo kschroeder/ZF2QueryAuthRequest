718d20642cb0f53fc5256c0d6de0ecc20c094bb2
Kevin Schroeder <kschroeder@mirageworks.com>
2016-03-21 13:04:15 -0500

    Initial commit

---
+++ lib/Incoming/Zf2RequestAdapter.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Eschrade\QueryAuth\Request\Incoming;
+
+use QueryAuth\Request\IncomingRequestInterface;
+use QueryAuth\Request\RequestInterface;
+use Zend\Http\Request;
+
+class Zf2RequestAdapter  implements IncomingRequestInterface, RequestInterface
+{
+
+    protected $request;
+
+    public function __construct(
+        Request $request
+    )
+    {
+        $this->request = $request;
+    }
+
+    public function getMethod()
+    {
+        return $this->request->getMethod();
+    }
+
+    public function getHost()
+    {
+        return $this->request->getUri()->getHost();
+    }
+
+    public function getPath()
+    {
+        $this->request->getUri()->getPath();
+    }
+
+    public function getParams()
+    {
+        if ($this->getMethod() === Request::METHOD_GET) {
+            return $this->request->getQuery();
+        }
+
+        return $this->request->getPost();
+    }
+
+
+}
